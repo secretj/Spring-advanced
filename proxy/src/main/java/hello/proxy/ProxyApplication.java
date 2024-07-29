ee15df65ab7d395aecb26e87730d023a126fbeb5
qkrwlsgud890 <qkrwlsgud890@gmail.com>
2024-07-30 01:33:06 +0900

    feat : 인터페이스 있는 형태, 없는 형태, 자동 컴포넌트 스캔/수동 컴포넌트 스캔

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,13 +1,16 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1config;
+import hello.proxy.config.AppV2config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import({AppV1config.class, AppV2config.class})
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") // 해당 루트 패키지 이하의 모든 빈을 스캔한다.
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
-
 }
--- proxy/src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ proxy/src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping // 스프링은 @Controller 또는 @RequestMapping 이 있어야 스프링 컨트롤러로 인식한다.
+/**
+ * 스프링은 @Controller 또는 @RequestMapping 이 있어야 스프링 컨트롤러로 인식한다.
+ * 하지만 @Controller는 자동 컴포넌트 스캔의 대상이 되기 때문에, 수동 Bean 등록을 하려면 @RequestMapping을 사용해야 한다.
+ */
+@RequestMapping
 @ResponseBody
 public interface OrderControllerV1 {
 
     @GetMapping("/v1/request")
-    String request(@RequestParam("itemId") String itemId);
+    String request(@RequestParam("itemId") String itemId);  //인터페이스에는 @RequestParam을 안 넣어주면 컴파일할 때, 인식 못할때가 있다.
 
     @GetMapping("v1/no-log")
     String noLog();
