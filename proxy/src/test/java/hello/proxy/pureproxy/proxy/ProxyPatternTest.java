03d23bc9952012ad270f825cc427cdd91b9b1b39
qkrwlsgud890 <qkrwlsgud890@gmail.com>
2024-07-30 02:07:19 +0900

    feat(proxy) : proxy 캐싱 테스트 코드

--- proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,17 @@ public class ProxyPatternTest {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
         // proxy가 없으니 캐싱이 되지 않고, 개당 1초씩 소모됨 - 불 필요한 리소스
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        // proxy가 있으니 캐싱이 되어, 1초 소모됨
         client.execute();
         client.execute();
         client.execute();
